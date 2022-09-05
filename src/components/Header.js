50cf849ecc8211e6fd11b875d9240682339e43df
Micaella Larissa <micaella.r@aluno.ifsp.edu.br>
2022-09-05 17:42:01 -0300

    Criação do Menu e Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,23 @@
 import './Header.css'
+import {GiFilmSpool as Film} from 'react-icons/gi'
+import { NavLink, Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div className='Header'>
         <div className='container'>
-          <div>Logo</div>
-          <div>Menu</div>
+          <Link to='/'>
+            <div className='logotype'>
+              <Film className='icon-logo'/>
+              <h1>React Tutorial</h1>
+            </div>
+          </Link>
+          <nav>
+            <ul>
+              <li><NavLink to='/'>Home</NavLink></li>
+              <li><NavLink to='/contact'>Hooks</NavLink></li>
+            </ul>
+          </nav>
         </div>
     </div>
   )
