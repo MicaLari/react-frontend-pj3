7cda671247680da1ba3a2fb47b85cc31ac6f3f41
Micaella Larissa <micaella.r@aluno.ifsp.edu.br>
2022-09-05 17:43:41 -0300

    Import se footer e card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,9 @@
 import {Link} from 'react-router-dom'
-import Header from "../components/Header"
 import Footer from "../components/Footer"
+import Header from "../components/Header"
 import "./Home.css"
 import MainContainer from '../components/MainContainer'
+import CardComment from '../components/CardComment'
 
 const Home = () => {
   return (
@@ -18,9 +19,21 @@ const Home = () => {
 
           <p>Em 2015, o Facebook anunciou o módulo React Native, que em conjunto com o React, possibilita o desenvolvimento de aplicativos para Android e iOS utilizando componentes de interface de usuário nativos de ambas plataformas, sem precisar recorrer ao HTML.</p>
 
-          <p>Na pesquisa de 2018 sobre hábitos de desenvolvedores do site Stack Overflow, o React foi a terceira biblioteca ou framework mais citado pelos usuários e desenvolvedores profissionais, ficando atrás somente do Node.js e Angular, respectivamente.</p>
+          <p>
+            Na pesquisa de 2018 sobre hábitos de desenvolvedores do site Stack Overflow, o React foi a terceira biblioteca ou framework mais citado pelos usuários e desenvolvedores profissionais, ficando atrás somente do Node.js e Angular, respectivamente.
+          </p>
                 
+        <CardComment initial='R' name='Renan'>
+          Na pesquisa de 2018 sobre hábitos de desenvolvedores do site Stack Overflow, o React foi a terceira
+        </CardComment>
 
+        <CardComment initial='M' name='Maria'>
+          Profissionais, ficando atrás somente do Node.js e Angular
+        </CardComment>
+
+        <CardComment initial='C' name='Carmo'>
+          Fiquei amigo do react!
+        </CardComment>
         
    
       </MainContainer>
