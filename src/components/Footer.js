5288ffd56d24de8778638ee65048ea3653a9f33a
Micaella Larissa <micaella.r@aluno.ifsp.edu.br>
2022-09-05 15:10:38 -0300

    Criação de MainContainer,etc..

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import './Footer.css'
 const Footer = () => {
   return (
     <footer className='main-footer'>
-    <p>© 2022 Micaella Larissa</p>
-</footer>
+      <p>© 2022 Micaella Larissa</p>
+    </footer>
   )
 }
 
