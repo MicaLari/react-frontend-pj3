b64e425dea3355b24348e95ef05a97a79111eaee
Micaella <micaella.r@aluno.ifsp.edu.br>
2022-09-12 17:29:58 -0300

    Comment and useEffect

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,19 @@
 import { Link } from 'react-router-dom'
+import {useState, useEffect} from 'react'
 import Footer from "../components/Footer"
 import Header from "../components/Header"
 import "./Contact.css"
 import MainContainer from '../components/MainContainer'
-import { useState } from 'react'
+
 
 const Contact = () => {
   
   const [count, setCount] = useState(10)
 
-  const [nameUser, setNameUser] = useState("Micaella")
+  
+  useEffect(() => {
+    console.log(count)
+  },[count])
 
   return (
     <>
@@ -18,8 +22,8 @@ const Contact = () => {
         <h1> My number is: {count}</h1>
         <button onClick={() => setCount(count + 1)}>Plus 1</button>
 
-        <h1> My name is: {nameUser}</h1>
-        <button onClick={() => setNameUser("Karla")}>Plus</button><br />
+        {/* <h1> My name is: {nameUser}</h1>
+        <button onClick={() => setNameUser("Karla")}>Plus</button><br /> */}
         {/* <Link to='/'>Home</Link>
         <h1>Contact</h1>
 
