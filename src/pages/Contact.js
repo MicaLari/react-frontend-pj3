7dba051d99e13ef06b13d37cc9be5b1e258b8f95
Micaella <micaella.r@aluno.ifsp.edu.br>
2022-09-12 16:53:40 -0300

    cooke and setCount

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,16 +3,27 @@ import Footer from "../components/Footer"
 import Header from "../components/Header"
 import "./Contact.css"
 import MainContainer from '../components/MainContainer'
+import { useState } from 'react'
 
 const Contact = () => {
+  
+  const [count, setCount] = useState(10)
+
+  const [nameUser, setNameUser] = useState("Micaella")
+
   return (
     <>
       <Header />
       <MainContainer>
-        <Link to='/'>Home</Link>
+        <h1> My number is: {count}</h1>
+        <button onClick={() => setCount(count + 1)}>Plus 1</button>
+
+        <h1> My name is: {nameUser}</h1>
+        <button onClick={() => setNameUser("Karla")}>Plus</button><br />
+        {/* <Link to='/'>Home</Link>
         <h1>Contact</h1>
 
-        <p>Aqui é o contact</p>
+        <p>Aqui é o contact</p> */}
         
       </MainContainer>
       <Footer />
