fdb5891043bb856111a1b97b61c4efb1740a1adf
Micaella <micaella.r@aluno.ifsp.edu.br>
2022-09-19 15:15:31 -0300

    mudanca de past

--- src/components/CardComment.js
+++ src/components/CardComment.js
@@ -1,49 +1,19 @@
-
+import './Card.css'
 const CardComment = ({initial, name, children}) => {
 
-    const {card, avatar, box, userName, text} = style
+    // const {card, avatar, box, userName, text} = style
   
     return (
-      <div style={card}>
-          <div style={avatar}>{initial}</div>
-          <div style={box}>
-              <h1 style={userName}>{name}</h1>
-              <p style={text}>{children}</p>
+      <div>
+          <div className='avatar'>{initial}</div>
+          <div className='box'>
+              <h1 className='userName'>{name}</h1>
+              <p className='text'>{children}</p>
           </div>
       </div>
     )
   }
   
-  const style = {
-      avatar: {
-          background: '#CCC',
-          borderRadius: '50%',
-          height: '50px',
-          width: '50px',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          margin: '0 10px 0 0'
-      },
-      card: {
-          width: '300px',
-          background: '#EEE',
-          display: 'flex',
-          padding: '10px',
-          borderRadius: '10px',
-          margin: '15px'
-      },
-      userName: {
-          margin: 0,
-          fontSize: '0.9rem'
-      },
-      text: {
-          margin: 0
-      },
-      box: {
-          width: '235px'
-      }
   
-  }
   
   export default CardComment
