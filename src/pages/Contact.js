891d3d60f108adafebe0f9651b887c083ed3d263
Micaella <micaella.r@aluno.ifsp.edu.br>
2022-09-19 17:06:41 -0300

    add de import  and onClick

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,13 @@
-import { Link } from 'react-router-dom'
 import {useState, useEffect} from 'react'
-import Footer from "../components/Footer"
 import Header from "../components/Header"
 import "./Contact.css"
 import MainContainer from '../components/MainContainer'
-
+import Footer from '../components/Footer'
 
 const Contact = () => {
   
-  const [count, setCount] = useState(10)
-
+  const [count, setCount] = useState(0)
+  const [nameUser, setNameUser] = useState("Micaella")
   
   useEffect(() => {
     console.log(count)
@@ -19,16 +17,11 @@ const Contact = () => {
     <>
       <Header />
       <MainContainer>
-        <h1> My number is: {count}</h1>
+        <h1>Olá {nameUser}!</h1>
+        <h1>My number is: {count}</h1>
         <button onClick={() => setCount(count + 1)}>Plus 1</button>
-
-        {/* <h1> My name is: {nameUser}</h1>
-        <button onClick={() => setNameUser("Karla")}>Plus</button><br /> */}
-        {/* <Link to='/'>Home</Link>
-        <h1>Contact</h1>
-
-        <p>Aqui é o contact</p> */}
-        
+        <br/><br/>
+        <button onClick={() => setNameUser("Marcos")}>Change name Marcos</button>
       </MainContainer>
       <Footer />
     </>
