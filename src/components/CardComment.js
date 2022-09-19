0bb31bb923a074e19727f21d86345749a8f61a28
Micaella <micaella.r@aluno.ifsp.edu.br>
2022-09-19 17:03:16 -0300

    add de img

--- src/components/CardComment.js
+++ src/components/CardComment.js
@@ -1,11 +1,11 @@
 import './Card.css'
-const CardComment = ({initial, name, children}) => {
+const CardComment = ({avatarUrl, name, children}) => {
 
     // const {card, avatar, box, userName, text} = style
   
     return (
-      <div>
-          <div className='avatar'>{initial}</div>
+      <div className='card'>
+          <img src={avatarUrl} className='avatar' alt='Foto do Fulano'/>
           <div className='box'>
               <h1 className='userName'>{name}</h1>
               <p className='text'>{children}</p>
