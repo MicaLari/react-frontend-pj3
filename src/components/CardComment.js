e4d96a4b614f878361c87af75d9f40c9c63ef482
Micaella <micaella.r@aluno.ifsp.edu.br>
2022-09-20 16:27:44 -0300

    import of link and add id

--- src/components/CardComment.js
+++ src/components/CardComment.js
@@ -1,16 +1,20 @@
+import { Link } from "react-router-dom"
 import './Card.css'
-const CardComment = ({avatarUrl, name, children}) => {
+
+const CardComment = ({avatarUrl, name, children, id}) => {
 
     // const {card, avatar, box, userName, text} = style
   
     return (
-      <div className='card'>
-          <img src={avatarUrl} className='avatar' alt='Foto do Fulano'/>
-          <div className='box'>
-              <h1 className='userName'>{name}</h1>
-              <p className='text'>{children}</p>
+        <Link to={`/user/${id}`}>
+          <div className='card'>
+              <img src={avatarUrl} className='avatar' alt='Foto do Fulano'/>
+              <div className='box'>
+                  <h1 className='userName'>{name}</h1>
+                  <p className='text'>{children}</p>
+              </div>
           </div>
-      </div>
+        </Link>
     )
   }
   
